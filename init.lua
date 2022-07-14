e9d46c9a0c2b337edbb7cd6d09fe3e12c295ff1b
naman2003now <a6.namanagrawal@gmail.com>
2022-07-14 16:06:06 +0530

    Going good

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
-require("config.general")
+require "naman.options"
+require "naman.plugins"
+require "naman.plugins.telescope"
+require "naman.plugins.cmp"
+require "naman.keymaps"
+
+vim.cmd "colorscheme dracula"
