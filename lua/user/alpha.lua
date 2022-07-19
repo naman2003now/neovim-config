4af311cf97938330fdd076fe7fb0b58f33e6d4ed
naman2003now <a6.namanagrawal@gmail.com>
2022-07-19 09:14:47 +0530

    TSServer and Greeting message

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "You are in naman2003now's workspace"
 end
 
 dashboard.section.footer.val = footer()
