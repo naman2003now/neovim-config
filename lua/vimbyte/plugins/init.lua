2978694b52df5dbb07ad08d69a133835e1b5e056
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-07-02 12:47:26 +0530

    Automatic plugin config and which-key mappings
    - Packer automatically reads the plugin file names from a list and installs them
    - Which key mappings are automatically registered from mappings.lua file

--- lua/vimbyte/plugins/init.lua
+++ lua/vimbyte/plugins/init.lua
@@ -1,5 +1,13 @@
+function register_plugin(plugin_name, use)
+  local status_ok, plugin_config = pcall(require, "vimbyte.plugins." .. plugin_name)
+  if not status_ok then return end
+  use(plugin_config)
+  pcall(require, "vimbyte.plugins." .. plugin_name .. ".mappings")
+end
+
 return function(use) 
-  use(require "vimbyte.plugins.colorscheme")
-  use(require "vimbyte.plugins.treesitter")
-  use(require "vimbyte.plugins.which-key")
+  plugins = {"colorscheme", "treesitter", "which-key"}
+  for _, plugin_name in pairs(plugins) do
+    register_plugin(plugin_name, use)
+  end
 end
