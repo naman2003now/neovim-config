96507127ce3981b92eddfaa8640c73c8b7550c6c
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-07-07 17:46:23 +0530

    Changed Telescope Mapping
    Changed <leader>ff from git_files to find_files as the git files does not register files which are not yet commited.
    assigned git_files to <leader>fg and removed <leader>fa

--- lua/vimbyte/plugins/telescope/mappings.lua
+++ lua/vimbyte/plugins/telescope/mappings.lua
@@ -4,8 +4,8 @@ if not status_ok then return end
 wk.register({
   ["f"] = {
     name = "Find through telescope", 
-    a = { "<cmd>Telescope find_files<cr>", "Find all files" },
-    f = { "<cmd>Telescope git_files<cr>", "Find files in project" }
+    f = { "<cmd>Telescope find_files<cr>", "Find all files" },
+    g = { "<cmd>Telescope git_files<cr>", "Find files in git repository" },
     s = { "<cmd>Telescope spell_suggest<cr>", "Spelling suggestions" }
   }
 }, { mode = "n", prefix = "<leader>" })
